905aed5997f1929248e9d8faf3ee27e6c71bdc96
OmerKilic0 <omerfarukkilic.17@gmail.com>
2024-09-10 15:38:00 +0300

    Watched ve watchlist sayfaları eklendi

--- src/main/java/com/omer/sakila/movimo/controller/MenuController.java
+++ src/main/java/com/omer/sakila/movimo/controller/MenuController.java
@@ -1,11 +1,15 @@
 package com.omer.sakila.movimo.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.omer.sakila.movimo.entity.Actor;
 import com.omer.sakila.movimo.entity.Customer;
@@ -18,30 +22,109 @@ import com.omer.sakila.movimo.service.FilmService;
 public class MenuController {
 
 	@Autowired
-    private FilmService filmService;
+	private FilmService filmService;
 
-    @Autowired
-    private ActorService actorService;
-    
-    @Autowired
-    private CustomerService customerService;
-    
-    public MenuController(FilmService filmService, ActorService actorService, CustomerService customerService) {
-    	this.filmService = filmService;
-    	this.actorService = actorService;
-    	this.customerService = customerService;
-    }
-    
+	@Autowired
+	private ActorService actorService;
+
+	@Autowired
+	private CustomerService customerService;
+
+	public MenuController(FilmService filmService, ActorService actorService, CustomerService customerService) {
+		this.filmService = filmService;
+		this.actorService = actorService;
+		this.customerService = customerService;
+	}
+
 	@GetMapping("/")
 	public String showMenu(ModelMap model) {
 		List<Film> films = filmService.getAllFilms();
-        List<Actor> actors = actorService.getAllActors();
-        Customer customer = customerService.authenticateUser();
+		List<Actor> actors = actorService.getAllActors();
+		Customer customer = customerService.authenticateUser();
 
-        model.addAttribute("films", films);
-        model.addAttribute("actors", actors);
-        model.addAttribute("customer", customer);
+		Set<Integer> watchedFilmIds = customerService.getWatchedFilms(customer.getId()).stream().map(Film::getId)
+				.collect(Collectors.toSet());
 
+		Set<Integer> watchlistFilmIds = customerService.getWatchlistFilms(customer.getId()).stream().map(Film::getId)
+				.collect(Collectors.toSet());
+
+		model.addAttribute("films", films);
+		model.addAttribute("actors", actors);
+		model.addAttribute("customer", customer);
+		model.addAttribute("watched", watchedFilmIds);
+		model.addAttribute("inWatchlist", watchlistFilmIds);
 		return "menu";
+	}
+
+	@GetMapping("watched-films")
+	public String showWatchedFilms(ModelMap model) {
+		Customer customer = customerService.authenticateUser();
+		model.addAttribute(customer);
+		Set<Integer> watchedFilmIds = customerService.getWatchedFilms(customer.getId()).stream().map(Film::getId)
+				.collect(Collectors.toSet());
+		List<Film> watchedFilms = new ArrayList<>();
+
+		for (int watchedFilmId : watchedFilmIds) {
+			watchedFilms.add(filmService.getFilmById(watchedFilmId));
+		}
+
+		model.addAttribute("watchedFilms", watchedFilms);
+		return "watched-films";
+	}
+
+	@GetMapping("watched-films/search")
+	public String searchFilmsWatched(@RequestParam(required = false) String name, ModelMap model) {
+		Customer customer = customerService.authenticateUser();
+		model.addAttribute("customer", customer);
+		Set<Integer> watchedFilmIds = customerService.getWatchedFilms(customer.getId()).stream().map(Film::getId)
+				.collect(Collectors.toSet());
+		List<Film> watchedFilms = new ArrayList<>();
+
+		for (int watchedFilmId : watchedFilmIds) {
+			watchedFilms.add(filmService.getFilmById(watchedFilmId));
+		}
+
+		if (name != null && !name.isEmpty()) {
+			model.addAttribute("watchedFilms", filmService.searchFilmsByName(name));
+		} else {
+			model.addAttribute("watchedFilms", watchedFilms);
+		}
+		return "watched-films";
+	}
+	
+	@GetMapping("watchlist")
+	public String showWatchist(ModelMap model) {
+		Customer customer = customerService.authenticateUser();
+		model.addAttribute(customer);
+		Set<Integer> watchlistFilmIds = customerService.getWatchlistFilms(customer.getId()).stream().map(Film::getId)
+				.collect(Collectors.toSet());
+		List<Film> watchlist = new ArrayList<>();
+
+		for (int watchlistFilmId : watchlistFilmIds) {
+			watchlist.add(filmService.getFilmById(watchlistFilmId));
+		}
+
+		model.addAttribute("watchlist", watchlist);
+		return "watchlist";
+	}
+
+	@GetMapping("watchlist/search")
+	public String searchFilmsWatchlist(@RequestParam(required = false) String name, ModelMap model) {
+		Customer customer = customerService.authenticateUser();
+		model.addAttribute("customer", customer);
+		Set<Integer> watchlistFilmIds = customerService.getWatchlistFilms(customer.getId()).stream().map(Film::getId)
+				.collect(Collectors.toSet());
+		List<Film> watchlist = new ArrayList<>();
+
+		for (int watchlistFilmId : watchlistFilmIds) {
+			watchlist.add(filmService.getFilmById(watchlistFilmId));
+		}
+
+		if (name != null && !name.isEmpty()) {
+			model.addAttribute("watchlist", filmService.searchFilmsByName(name));
+		} else {
+			model.addAttribute("watchlist", watchlist);
+		}
+		return "watchlist";
 	}
 }
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/watchlist.jsp
@@ -0,0 +1,39 @@
+<%@ include file="/WEB-INF/jsp/common/header.jspf"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Movimo - Watchlist</title>
+<link rel="stylesheet"
+	href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+<link rel="stylesheet"
+	href="${pageContext.request.contextPath}/css/styles.css">
+<link rel="stylesheet"
+	href="${pageContext.request.contextPath}/css/category-details.css">
+
+</head>
+<body>
+	<div class="search-container">
+		<form action="${pageContext.request.contextPath}/watchlist/search" method="get">
+			<input type="text" name="name" placeholder="Search for films..."
+				value="${param.name}" />
+		</form>
+	</div>
+
+	<h2>Watchlist</h2>
+
+	<div class="film-container">
+		<c:forEach var="film" items="${watchlist}">
+			<div class="film-card"
+				onclick="window.location.href='${pageContext.request.contextPath}/films/${film.title}'">
+				<img
+					src="${pageContext.request.contextPath}/images/films/poster${film.id}.jpg"
+					alt="${film.title} Poster">
+				<h3>${film.title}</h3>
+			</div>
+		</c:forEach>
+	</div>
+</body>
+</html>
